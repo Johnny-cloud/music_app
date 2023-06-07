cf0566b2e55e35e6965f1c5084b1b99fc4c4e3b7
John Kipruto <johnkipruto157@gmail.com>
2023-06-07 23:43:23 +0300

    Add CORS to connect to frontend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,8 +4,29 @@ class ApplicationController < Sinatra::Base
         "<h1> Welcome to my music app </h1>"
     end
 
+    get "/trendings" do 
+        Trending.all().to_json
+    end
+
+    get "/top10s" do 
+        Top10.all.to_json
+    end
+
+    get "/artists" do 
+        Artist.all.to_json
+    end
+
     get "/artists/:id/albums" do 
         artist = Artist.find(params[:id])
         artist.albums.to_json
+    end
+
+    get "/albums" do 
+        Album.all.to_json
+    end
+
+    get "/albums/:id/songs" do 
+        album = Album.find(params[:id])
+        album.songs.to_json
     end
 end
